5ac4c4a6d69cc328ffdecea04d4b302a1ff55b9c
fchuseynli <hseynli@list.ru>
2023-05-02 11:39:09 +0400

    fixed bug in the task

--- Lesson9.Inheritance/17. Tasks/Program.cs
+++ Lesson9.Inheritance/17. Tasks/Program.cs
@@ -2,7 +2,7 @@
  1. Person adında klas yaradın, iki digər klas Persondan inherit olacaq: Teacher və Student. Ümumi Person klasına aid olanları Person klasına daxil edin, misal üçün Age, Height, HairColor və s. 
 Teacher klasına aid olanları isə həmin klasa daxil edin, misal üçün LessonsInAWeek, Salary və s. S
 Student klasına isə ona aid olan propery və metodları daxil edin. Misal üçün UniversityName, FrientCount və s
-2. Yuxarıda yaradılan Student klasından ibarət 3 ölçülü array yaradın. Həmin arrayın elementlərini (propery-lərini) istifadəçi ekrandan daxil etsin. Yekundan məlumatları ekranda əks elətdiri.
+2. Yuxarıda yaradılan Student klasından ibarət 2 ölçülü 3 elementdən ibarət array yaradın. Həmin arrayın elementlərini (propery-lərini) istifadəçi ekrandan daxil etsin. Yekundan məlumatları ekranda əks elətdiri.
  */
 
 Console.WriteLine("\nDone!");
